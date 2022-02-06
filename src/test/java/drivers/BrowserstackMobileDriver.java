c19019cb41080522e6f0481906856e0794fae0b9
Konyaz <80565760+Konyaz@users.noreply.github.com>
2022-02-07 00:24:59 +0300

    tests

---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,43 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
+
+        // Set your access credentials
+        desiredCapabilities.setCapability("browserstack.user", "xegimmannawocom_usrySY");
+        desiredCapabilities.setCapability("browserstack.key", "qYxZv2PgY6VUwC3vHYxu");
+
+        // Set URL of the application under test
+        desiredCapabilities.setCapability("app", "bs://1396f9ab9dc72a189ad87a8df9986351e8fb11dc");
+
+        // Specify device and os_version for testing
+        desiredCapabilities.setCapability("device", "Google Pixel 3");
+        desiredCapabilities.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        desiredCapabilities.setCapability("project", "First Java Project");
+        desiredCapabilities.setCapability("build", "Java Android");
+        desiredCapabilities.setCapability("name", "first_test");
+
+        return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
+    }
+}
---
+++ src/test/java/tests/BrowserstackTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
+@Tag("selenide_android")
+public class BrowserstackTests extends TestBase {
+
+    @Test
+    @DisplayName("Successful tests in GoogleTranslate android app")
+    void searchTest() {
+        step("Check input", () -> {
+
+            $(MobileBy.id("com.google.android.apps.translate:id/button_done")).click();
+            $(MobileBy.id("com.google.android.apps.translate:id/design_menu_item_text")).click();
+            $(MobileBy.id("com.google.android.apps.translate:id/touch_to_type_text")).click();
+            $(MobileBy.id("com.google.android.apps.translate:id/edit_input")).val("Hello");
+            $(MobileBy.id("com.google.android.apps.translate:id/result_selector")).click();
+            $(MobileBy.id("android:id/text1")).shouldHave(text("Hello"));
+        });
+        step("Switch language check", () -> {
+            $(MobileBy.id("com.google.android.apps.translate:id/btn_lang_picker_swap")).click();
+            $(MobileBy.id("com.google.android.apps.translate:id/picker1")).shouldHave(text("Spanish"));
+
+
+        });
+
+    }
+}
